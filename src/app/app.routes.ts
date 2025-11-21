eaf3be3d0327fb401b80bcec2c7e8bbfaa50ddc4
Gedico <Gedico3@gmail.com>
2025-11-21 15:38:24 +0100

    commit merge con nives del 21/11

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,52 +1,51 @@
 import { Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
 import { LoginGuard } from './guards/login.guard';
-import { VisualizzaInserzioneComponent } from './pages/visualizza-inserzione/visualizza-inserzione.component';
+
 
 export const routes: Routes = [
-  {
-    path: 'register',
-    loadComponent: () =>
-      import('./pages/auth/register/register.component')
-        .then(m => m.RegisterComponent),
+
+/*-------------------------------LANDING---------------------------------------------------------------------------- */
+
+// Landing Page
+  { path: '',
+    loadComponent: () =>import('./pages/landing/landing.component')
+    .then(m => m.LandingPageComponent)
+  },
+
+
+/*-------------------------------AUTH---------------------------------------------------------------------------- */
+
+  // Register
+  { path: 'register',
+    loadComponent: () =>import('./pages/auth/register/register.component')
+    .then(m => m.RegisterComponent),
     canActivate: [LoginGuard]
-  }
-  ,
+  },
 
-  {
-    path: '',
-    loadComponent: () =>
-    import('./pages/landing/landing.component')
-      .then(m => m.LandingPageComponent)
-  }
-  ,
 
-  {
-  path: 'oauth-callback',
-  loadComponent: () =>
-    import('./pages/auth/oauth-callback/oauth-callback.component')
-      .then(m => m.OAuthCallbackComponent)
-  }
- ,
+  // OAuth Callback
+  { path: 'oauth-callback',
+    loadComponent: () =>import('./pages/auth/oauth-callback/oauth-callback.component')
+    .then(m => m.OAuthCallbackComponent)
+  },
+ 
+  // Login
+  { path: 'login',
+    loadComponent: () =>import('./pages/auth/login/login.component')
+    .then(m => m.LoginComponent),
+    canActivate: [LoginGuard]
+  },
 
-  {
-  path: 'login',
-  loadComponent: () =>
-    import('./pages/auth/login/login.component')
-      .then(m => m.LoginComponent),
-  canActivate: [LoginGuard]
-  }
-
-  ,
+/*-------------------------------INSERZIONI---------------------------------------------------------------------------- */
+  // InserzionE
+  { path: 'inserzione/:id', 
+    loadComponent: () =>import('./pages/visualizza-inserzione/visualizza-inserzione.component')
+    .then(m => m.VisualizzaInserzioneComponent)
+  },
   
-  {
-
-    path: 'inserzione/:id', component: VisualizzaInserzioneComponent
-
-  }
   
-  ,
-
+/*-------------------------------REDIRECT GENERICO---------------------------------------------------------------------------- */
   {
   path: '**',
   redirectTo: '',
