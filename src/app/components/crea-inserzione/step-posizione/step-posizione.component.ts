9e2e5fd7fc32818c3f748f9afad13f869599382c
Gedico <Gedico3@gmail.com>
2025-12-17 17:08:34 +0100

    commit 17/12 post @if

--- src/app/components/crea-inserzione/step-posizione/step-posizione.component.ts
+++ src/app/components/crea-inserzione/step-posizione/step-posizione.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild, ElementRef } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormGroup, ReactiveFormsModule} from '@angular/forms';
 
